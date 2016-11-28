189dd931e6cf3ccc8e166675fbca3ba9aa5985e3
liushibao <1397129639@qq.com>
2016-11-28 11:58:09 +0800

    全站

---
+++ Application/User/Conf/config.php
@@ -0,0 +1,19 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
+// +----------------------------------------------------------------------
+
+/**
+ * UCenter客户端配置文件
+ * 注意：该配置文件请使用常量方式定义
+ */
+
+define('UC_APP_ID', 1); //应用ID
+define('UC_API_TYPE', 'Model'); //可选值 Model / Service
+define('UC_AUTH_KEY', 'f]h7IR?Wz8:"k-Y#XASqMaE%Hng`[4P16QCG50,&'); //加密KEY
+define('UC_DB_DSN', 'mysqli://root:root@127.0.0.1:3306/finance'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_TABLE_PREFIX', 'finance_'); // 数据表前缀，使用Model方式调用API必须配置此项
